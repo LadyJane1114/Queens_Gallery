dffbe062e2f7685c94fa578c4b2c2f1eaf242722
LadyJane1114 <sweet.jane.s@outlook.com>
2026-01-10 16:03:08 -0400

    Updating Model

--- WPF-basics-lab/Models/CarouselItem.cs
+++ WPF-basics-lab/Models/CarouselItem.cs
@@ -8,5 +8,10 @@ namespace Queens_Gallery.Models
     {
         public string Title { get; set; } = string.Empty;
         public string ImagePath { get; set; } = string.Empty;
+        public string Subtitle { get; set; } = string.Empty;
+        public string ActiveEra { get; set; } = string.Empty;
+        public string PrimaryLocation { get; set; } = string.Empty;
+        public string InfoBlurb { get; set; } = string.Empty;
+        public string wikiURL {  get; set; } = string.Empty;
     }
 }
